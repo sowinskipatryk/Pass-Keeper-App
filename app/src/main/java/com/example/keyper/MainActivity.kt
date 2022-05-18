b3a89c47056065cb7f01362042987cd9dfff521b
sowinskipatryk <sowinski.patryk@wp.pl>
2022-05-18 23:34:34 +0200

    attached login screen

---
+++ app/src/main/java/com/example/keyper/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.keyper
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.keyper.ui.login.LoginActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val goToLoginScreenButton = findViewById<Button>(R.id.goToLoginScreenButton)
+
+        goToLoginScreenButton.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}

907273614fb3f6bd393ad7c3203d6b35775a21f2
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2022-06-23 23:51:23 +0200

    delay on splash screen to load custom font

--- app/src/main/java/com/example/keyper/SplashScreenActivity.kt
+++ app/src/main/java/com/example/keyper/SplashScreenActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.View.VISIBLE
+import android.widget.ImageView
+import android.widget.TextView
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +15,15 @@ class SplashScreenActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
+        val textView = findViewById<TextView>(R.id.textView)
+        val imageView = findViewById<ImageView>(R.id.imageView)
         val handler = Handler()
+
+        handler.postDelayed({
+            textView.text = getString(R.string.app_name)
+            imageView.visibility = VISIBLE
+        }, 500)
+
         handler.postDelayed({
             val intent = Intent(this, LoginScreenActivity::class.java)
             startActivity(intent)

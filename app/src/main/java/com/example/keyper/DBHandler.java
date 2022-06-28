dc19d0940d5b0fde26d59ac818065b01aba419d4
sowinskipatryk <91700001+sowinskipatryk@users.noreply.github.com>
2022-06-28 22:28:57 +0200

    data encryption

--- app/src/main/java/com/example/keyper/DBHandler.java
+++ app/src/main/java/com/example/keyper/DBHandler.java
@@ -6,12 +6,17 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.scottyab.aescrypt.AESCrypt;
+
+import java.security.GeneralSecurityException;
 import java.util.ArrayList;
 
 public class DBHandler extends SQLiteOpenHelper {
     public DBHandler(Context context) {
         super(context, "servicedata.db", null, 1);
     }
+
+    public String cryptCode = "SuperSecretPassword";
 
     @Override
     public void onCreate(SQLiteDatabase db) {
@@ -32,8 +37,15 @@ public class DBHandler extends SQLiteOpenHelper {
 
         if (cursor.moveToFirst()) {
             do {
-                services.add(new Service(cursor.getString(0),
-                        cursor.getString(1)));
+                String encryptedName = cursor.getString(0);
+                String encryptedPassword = cursor.getString(1);
+                try {
+                    String decryptedName = AESCrypt.decrypt(cryptCode, encryptedName);
+                    String decryptedPassword = AESCrypt.decrypt(cryptCode, encryptedPassword);
+                    services.add(new Service(decryptedName, decryptedPassword));
+                } catch (GeneralSecurityException e) {
+                    e.printStackTrace();
+                }
             } while (cursor.moveToNext());
         }
         cursor.close();
@@ -42,72 +54,112 @@ public class DBHandler extends SQLiteOpenHelper {
 
     public Boolean equalsMasterKey(String password) {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor = db.rawQuery("SELECT password FROM Master WHERE name = ?", new String[] {"Key"});
-
-        if (cursor.moveToFirst()) {
-            String str = cursor.getString(cursor.getColumnIndexOrThrow("password"));
-            cursor.close();
-            return password.equals(str);
-        } else {
+        try {
+            String encryptedName = AESCrypt.encrypt(cryptCode, "Key");
+            String encryptedPassword = AESCrypt.encrypt(cryptCode, password);
+            Cursor cursor = db.rawQuery("SELECT password FROM Master WHERE name = ?", new String[] {encryptedName});
+            if (cursor.moveToFirst()) {
+                String str = cursor.getString(cursor.getColumnIndexOrThrow("password"));
+                cursor.close();
+                return encryptedPassword.equals(str);
+            } else {
+                return false;
+            }
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
             return false;
         }
     }
 
     public Boolean setMasterKey() {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT password FROM Master WHERE name = ?", new String[] {"Key"});
-        if (cursor.moveToFirst()) {
+        try {
+            String encryptedName = AESCrypt.encrypt(cryptCode, "Key");
+            String encryptedPassword = AESCrypt.encrypt(cryptCode, "1234");
+            Cursor cursor = db.rawQuery("SELECT password FROM Master WHERE name = ?", new String[] {encryptedName});
+            if (cursor.moveToFirst()) {
+                return false;
+            } else {
+                ContentValues contentValues = new ContentValues();
+                contentValues.put("name", encryptedName);
+                contentValues.put("password", encryptedPassword);
+                long result = db.insert("Master", null, contentValues);
+                return result != -1;
+            }
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
             return false;
-        } else {
-            ContentValues contentValues = new ContentValues();
-            contentValues.put("name", "Key");
-            contentValues.put("password", "1234");
-            long result = db.insert("Master", null, contentValues);
-            return result != -1;
         }
     }
 
     public Boolean updateMasterKey(String password) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("name", "Key");
-        contentValues.put("password", password);
-        long result = db.update("Master", contentValues, "name=?", new String[] {"Key"});
-        return result != -1;
+        try {
+            String encryptedName = AESCrypt.encrypt(cryptCode, "Key");
+            String encryptedPassword = AESCrypt.encrypt(cryptCode, password);
+            SQLiteDatabase db = this.getWritableDatabase();
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("name", encryptedName);
+            contentValues.put("password", encryptedPassword);
+            long result = db.update("Master", contentValues, "name=?", new String[] {encryptedName});
+            return result != -1;
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+            return false;
         }
+    }
 
     public Boolean insertServicePassword(String name, String password) {
             SQLiteDatabase db = this.getWritableDatabase();
             ContentValues contentValues = new ContentValues();
-            contentValues.put("name", name);
-            contentValues.put("password", password);
-            long result = db.insert("Services", null, contentValues);
-        return result != -1;
+            try {
+                String encryptedName = AESCrypt.encrypt(cryptCode, name);
+                String encryptedPassword = AESCrypt.encrypt(cryptCode, password);
+                contentValues.put("name", encryptedName);
+                contentValues.put("password", encryptedPassword);
+                long result = db.insert("Services", null, contentValues);
+                return result != -1;
+            } catch (GeneralSecurityException e) {
+                e.printStackTrace();
+                return false;
+            }
     }
 
     public Boolean updateServicePassword(String name, String password) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put("name", name);
-        contentValues.put("password", password);
-        Cursor cursor = db.rawQuery("SELECT * FROM Services WHERE name = ?", new String[] {name});
-        if (cursor.getCount()>0) {
-            cursor.close();
-            long result = db.update("Services", contentValues, "name=?", new String[] {name});
-            return result != -1;
-        } else {
+        try {
+            String encryptedName = AESCrypt.encrypt(cryptCode, name);
+            String encryptedPassword = AESCrypt.encrypt(cryptCode, password);
+            contentValues.put("name", encryptedName);
+            contentValues.put("password", encryptedPassword);
+            Cursor cursor = db.rawQuery("SELECT * FROM Services WHERE name = ?", new String[] {encryptedName});
+            if (cursor.getCount()>0) {
+                cursor.close();
+                long result = db.update("Services", contentValues, "name=?", new String[] {encryptedName});
+                return result != -1;
+            } else {
+                return false;
+            }
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
             return false;
         }
     }
 
     public Boolean deleteServicePassword(String name) {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery("SELECT * FROM Services WHERE name = ?", new String[] {name});
-        if (cursor.getCount()>0) {
-            cursor.close();
-            long result = db.delete("Services", "name=?", new String[] {name});
-            return result != -1;
-        } else {
+        try {
+            String encryptedName = AESCrypt.encrypt(cryptCode, name);
+            Cursor cursor = db.rawQuery("SELECT * FROM Services WHERE name = ?", new String[] {encryptedName});
+            if (cursor.getCount()>0) {
+                cursor.close();
+                long result = db.delete("Services", "name=?", new String[] {encryptedName});
+                return result != -1;
+            } else {
+                return false;
+            }
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
             return false;
         }
     }
